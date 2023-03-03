3ca90186f37a3b9010ca271cb2f34890bf72fab7
bqyou <bingqianyou@gmail.com>
2023-03-03 09:12:46 +0800

    task 1

---
+++ src/test/java/tfip/nus/iss/pafassessment/PafassessmentApplicationTests.java
@@ -0,0 +1,13 @@
+package tfip.nus.iss.pafassessment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PafassessmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
